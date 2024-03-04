27b1316a2890173960b1e769a201ba8947c7bfc9
Jaeework <jaeework@gmail.com>
2024-03-05 05:32:08 +0900

    #5 part02 - `@RequestMapping`과 축약형 표현 비교
    스프링 4.3 이후 `@GetMapping` , `@PostMapping` 과 같은 축약형이 등장
    학습을 위해 비교 사용해 보는 코드 예제 작성

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -4,11 +4,26 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/sample/*")
 @Log4j2
 public class SampleController {
+
+    @RequestMapping(value="/basic", method = {RequestMethod.GET, RequestMethod.POST})
+    public void basicGet() {
+
+        log.info("basic get...............");
+
+    }
+
+    @GetMapping("/basicOnlyGet")
+    public void basicGet2() {
+
+        log.info("basic get only get.................");
+
+    }
 
     @RequestMapping("")
     public void basic() {
