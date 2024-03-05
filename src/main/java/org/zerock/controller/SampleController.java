bdcea2d6605fe14b2020b1000012fba37f236d56
Jaeework <jaeework@gmail.com>
2024-03-06 03:20:03 +0900

    #5 part02 - TodoDTO 도메인 추가 및 SampleController 코드 추가
    Date 타입으로의 변환과 같이 파라미터를 변환해서 처리해야 하는 경우의 학습을 위해
    - TodoDTO 도메인 추가
    - SampleController 에 예제 코드 추가
    * 컨트롤러에서 `@InitBinder` 를 통해 처리
    * 도메인 클래스에서 `@DateTimeFormat` 을 이용해 처리

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,14 +1,15 @@
 package org.zerock.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,6 +17,23 @@ import java.util.Arrays;
 @RequestMapping("/sample/*")
 @Log4j2
 public class SampleController {
+
+    /* 파라미터를 변환해서 처리해야 하는 경우 ex) Date 타입 */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        binder.registerCustomEditor(java.util.Date.class,
+                new CustomDateEditor(dateFormat, false));// true : null 값 허용, false : null 값 불허
+        // 커스텀 데이터 타입 변환 작업을 하는 데이터에 대해서 typeMismatch 의 경우 에러 메시지가 표시되도록 할 수 있음
+    }
+
+    @GetMapping("/ex03")
+    public String ex03(TodoDTO todo) {
+
+        log.info("todo : " + todo);
+
+        return "ex03";
+    }
 
     /* 여러 개의 객체 타입을 처리하는 경우 */
     @GetMapping("/ex02Bean")
