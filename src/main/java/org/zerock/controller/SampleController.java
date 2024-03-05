c10d5fb79e3acb640ba42e84eff0886986e8f22c
Jaeework <jaeework@gmail.com>
2024-03-06 01:28:13 +0900

    #5 part02 - SampleController 코드 추가
    동일한 이름의 파라미터가 여러 개 전달되는 경우의 학습을 위해 예제 코드 추가
    - List 처리
    - 배열 처리

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -8,11 +8,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("/sample/*")
 @Log4j2
 public class SampleController {
 
+    /* 동일한 이름의 파라미터가 여러 개 전달되는 경우 : 리스트, 배열 처리 */
+    @GetMapping("/ex02List")
+    public String ex02List(@RequestParam("ids")ArrayList<String> ids) {
+
+        log.info("ids : " + ids);
+
+        return "ex02List";
+    }
+
+    @GetMapping("/ex02Array")
+    public String ex02Array(@RequestParam("ids")String[] ids) {
+
+        log.info("array ids : " + Arrays.toString(ids));
+
+        return "ex02Array";
+    }
+
+    /* 기본 자료형이나 문자열 등 이용시, @RequestParam 이용해 파라미터의 타입만을 맞게 선언 */
     @GetMapping("/ex02")
     public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
 
@@ -22,6 +43,7 @@ public class SampleController {
         return "ex02";
     }
 
+    /* 파라미터의 수집 - 파라미터 타입에 따라 자동으로 변환*/
     @GetMapping("/ex01")
     public String ex01(SampleDTO dto) {
 
