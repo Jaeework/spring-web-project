c0f4641a921a12145fb93e63287ceb9318ff4dcb
Jaeework <jaeework@gmail.com>
2024-03-06 14:19:56 +0900

    #5 part02 - index.jsp 파일명 변경, HomeController 코드 추가
    - index.jsp 파일 home.jsp 으로 파일명 변경
    - Model 객체 이용해 view 에 데이터 전달

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -1,15 +1,31 @@
 package org.zerock.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/")
+@Log4j2
 public class HomeController {
 
-    @RequestMapping("")
-    public String home() {
-        return "index";
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        log.info("Welcome home! The client locale is {}.", locale);
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+        model.addAttribute("serverTime", formattedDate);
+
+        return "home";
     }
 
 }
