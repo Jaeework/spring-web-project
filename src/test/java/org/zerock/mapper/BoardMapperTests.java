20a58d684e0173d0c845129180b4c6befbd8a1cb
Jaeework <jaeework@gmail.com>
2024-03-13 17:25:21 +0900

    #9 part03 - create(insert) 처리
    Mapper 인터페이스에 메서드 추가 및 Mapper XML 에서 구현
    테스트 코드 작성
    
    * `@SelectKey` 를 이용하면 주로 PK와 같은 값을 미리(before) 혹은 후에(after) SQL 을 통해 처리해 두고 특정한 이름으로 결과를 보관한다.

--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -19,6 +20,30 @@ public class BoardMapperTests {
     @Test
     public void testGetlist() {
         mapper.getList().forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void testInsert() {
+        BoardVO board = new BoardVO();
+        board.setTitle("새로 작성하는 글");
+        board.setContent("새로 작성하는 내용");
+        board.setWriter("newbie");
+
+        mapper.insert(board);
+
+        log.info(board);
+    }
+
+    @Test
+    public void testInsertSelectKey() {
+        BoardVO board = new BoardVO();
+        board.setTitle("새로 작성하는 글 select Key");
+        board.setContent("새로 작성하는 내용 select Key");
+        board.setWriter("newbie");
+
+        mapper.insertSelectKey(board);
+
+        log.info(board);
     }
 
 }
