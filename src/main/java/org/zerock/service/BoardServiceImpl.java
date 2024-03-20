fea7fd3d01169181d33fa1e308d17b4e063f2184
Jaeework <jaeework@gmail.com>
2024-03-20 16:09:47 +0900

    #12 part03_business - BoardService, BoardServiceImpl 에 대한 구조 설정
    비즈니스 계층 구현을 위해 BoardService 인터페이스 및 구현클래스 BoardServiceImpl 의 기본 틀 작성

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,43 @@
+package org.zerock.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import java.util.List;
+
+@Log4j2
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService{
+
+    private BoardMapper mapper;
+
+    @Override
+    public void register(BoardVO board) {
+
+    }
+
+    @Override
+    public BoardVO get(Long bno) {
+        return null;
+    }
+
+    @Override
+    public boolean modify(BoardVO board) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Long bno) {
+        return false;
+    }
+
+    @Override
+    public List<BoardVO> getList() {
+        return null;
+    }
+
+}
