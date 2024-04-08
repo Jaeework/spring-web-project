f4f04376bbd7e99ff2fde6b3b1b30e7e7ee6f893
Jaeework <jaeework@gmail.com>
2024-04-08 14:54:59 +0900

    #18 part03_paging - Criteria 객체를 이용하여 Mapper 수정
    Criteria 객체를 이용하여 게시글 리스트를 받아올 수 있도록 Mapper 클래스와 XML에 sql 구문을 추가하고 정상적으로 동작하는지 테스트.

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package org.zerock.mapper;
 
 import org.apache.ibatis.annotations.Select;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import java.util.List;
 
@@ -9,6 +10,9 @@ public interface BoardMapper {
 
     //@Select("select * from tbl_board where bno > 0")
     public List<BoardVO> getList();
+
+    /* select list with paging */
+    public List<BoardVO> getListWithPaging(Criteria cri);
 
     /* create(insert) */
     public void insert(BoardVO board);
