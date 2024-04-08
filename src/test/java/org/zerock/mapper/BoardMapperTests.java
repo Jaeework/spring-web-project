cabcd7fcca9699f3ae95cc63a7bd9ee3f91ec3d0
Jaeework <jaeework@gmail.com>
2024-04-08 14:54:59 +0900

    #18 part03_paging - Criteria 객체를 이용하여 Mapper 수정
    Criteria 객체를 이용하여 게시글 리스트를 받아올 수 있도록 Mapper 클래스와 XML에 sql 구문을 추가하고 정상적으로 동작하는지 테스트.

--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -20,6 +23,19 @@ public class BoardMapperTests {
     @Test
     public void testGetlist() {
         mapper.getList().forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void testPaging() {
+        Criteria cri = new Criteria();
+
+        // 10개씩 3페이지
+        cri.setPageNum(3);
+        cri.setAmount(10);
+
+        List<BoardVO> list = mapper.getListWithPaging(cri);
+
+        list.forEach(board -> log.info(board));
     }
 
     @Test
