3fb99917c2235cb558fe45d173adcd3a96b2463b
Jaeework <jaeework@gmail.com>
2024-04-13 17:59:49 +0900

    #22 part04_Ajax - 특정 게시글의 댓글 리스트 가져오는 구문 작성 및 테스트
    특정 게시글에 연결된 댓글 리스트를 가져오는 구문 Mapper 에 작성 및 테스트 코드를 통한 확인

--- src/main/java/org/zerock/mapper/ReplyMapper.java
+++ src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -1,6 +1,10 @@
 package org.zerock.mapper;
 
+import org.apache.ibatis.annotations.Param;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
+
+import java.util.List;
 
 public interface ReplyMapper {
 
@@ -11,4 +15,9 @@ public interface ReplyMapper {
     public int delete(Long rno);
 
     public int update(ReplyVO vo);
+
+    public List<ReplyVO> getListWithPaging(
+            @Param("cri") Criteria cri,
+            @Param("bno") Long bno);
+
 }
