d5a21fbc4de32d613412bc103ccae11283fc86a1
jaysodev <jaysothedev@gmail.com>
2024-04-16 16:23:51 +0900

    #22 part04_Ajax - 댓글 단건 조회 처리
    특정 rno 을 가진 댓글을 조회해 오는 코드 작성

--- src/main/webapp/resources/js/reply.js
+++ src/main/webapp/resources/js/reply.js
@@ -80,11 +80,26 @@ var replyService = (function(){
         });
     }
 
+    function get(rno, callback, error) {
+
+        $.get("/replies/" + rno, function (result) {
+
+            if(callback) {
+                callback(result);
+            }
+        }).fail(function(xhr, status, err) {
+            if(error) {
+                error();
+            }
+        });
+    }
+
     return{
         add:add,
         getList:getList,
         remove:remove,
-        update:update
+        update:update,
+        get:get
     };
 
 })();
