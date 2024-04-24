1363f4fa3d8e130eb48199cc46481b0712b211a0
jaysodev <jaysothedev@gmail.com>
2024-04-24 17:44:40 +0900

    #34 part06 - 이미지 파일의 구분
    이미지 파일을 업로드 했을 때 화면에 썸네일을 보여주기 위해 썸네일 파일을 따로 저장하는 처리
    - `Thumbnailator` 라이브러리 사용 : 크기나 해상도를 직접 조절하는 작업을 줄이기 위해 사용
    - 라이브러리 사용을 위해 디펜던시 추가
    - 컨트롤러에서 이미지 타입의 파일을 판별하는 코드 추가 및 이미지 타입일 경우 썸네일 파일을 별도로 생성하는 코드 추가

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -1,6 +1,7 @@
 package org.zerock.controller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -83,11 +86,18 @@ public class UploadController {
 
             uploadFileName = uuid.toString() + "_" + uploadFileName;
 
-            //File saveFile = new File(uploadFolder, uploadFileName);
-            File saveFile = new File(uploadPath, uploadFileName);
-
             try {
+                File saveFile = new File(uploadPath, uploadFileName);
                 multipartFile.transferTo(saveFile);
+
+                // check image type file
+                if (checkImageType(saveFile)) {
+                    FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+
+                    Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+                    thumbnail.close();
+                }
+
             } catch (Exception e) {
                 log.error(e.getMessage());
             }
@@ -103,6 +113,19 @@ public class UploadController {
         String str = sdf.format(date);
 
         return str.replace("-", File.separator);
+    }
+
+    private boolean checkImageType(File file) {
+
+        try {
+            String contentType = Files.probeContentType(file.toPath());
+
+            return contentType.startsWith("image");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
 
